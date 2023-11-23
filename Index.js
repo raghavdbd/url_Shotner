c24a08e462453751a22e83053fdec22e529bca78
raghav goyal <raghavgoyaldbd4782gmail.com>
2023-11-23 09:09:01 +0530

    port change

--- Index.js
+++ Index.js
@@ -49,7 +49,7 @@ app.get('/:shortId', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7980;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
