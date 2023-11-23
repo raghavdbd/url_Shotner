4ed39c2748f4da2f9e60367fbd13c79861601e0b
raghav goyal <raghavgoyaldbd4782gmail.com>
2023-11-23 09:06:30 +0530

    make alias workable

--- Index.js
+++ Index.js
@@ -1,26 +1,35 @@
-//initializing the Express// 
+
+
+
 const express = require('express');
-//  nanoid is use to generate small compact id
 const { nanoid } = require('nanoid');
 
 const app = express();
 app.use(express.json());
 
-// this is used to url as key and shorter url as value
 const urlDatabase = {};
 
 app.post('/api/shorten', (req, res) => {
-    //  we get input from user
   const { url, customAlias } = req.body;
-//  if url is already present in databse we can just return the error message that alias exist
-  if (customAlias && urlDatabase[customAlias]) {
-    return res.status(400).json({ error: 'Custom alias already exists' });
+
+  if (customAlias) {
+    if (urlDatabase[customAlias]) {
+      return res.status(400).json({ error: 'Custom alias already exists' });
+    }
+
+    urlDatabase[customAlias] = {
+      url,
+      createdAt: Date.now(),
+      expiresIn: 2 * 60 * 60 * 1000, // Expiration time in milliseconds (2 hours)
+    };
+
+    return res.json({ originalUrl: url, shortenedUrl: customAlias });
   }
-//  it will check if custom alias is provided or not otherwise we can use unique id generate by nanoid
-  const shortId = customAlias || nanoid(6);
+
+  const shortId = nanoid(6);
   urlDatabase[shortId] = {
     url,
-    createdAt: Date.now(), // Store creation time
+    createdAt: Date.now(),
     expiresIn: 2 * 60 * 60 * 1000, // Expiration time in milliseconds (2 hours)
   };
 
@@ -31,7 +40,7 @@ app.post('/api/shorten', (req, res) => {
 app.get('/:shortId', (req, res) => {
   const { shortId } = req.params;
   const urlData = urlDatabase[shortId];
-//  it will url is still valid 
+
   if (urlData && Date.now() - urlData.createdAt < urlData.expiresIn) {
     res.redirect(urlData.url);
   } else {
@@ -46,3 +55,4 @@ app.listen(PORT, () => {
 });
 
 app.use(express.static('public'));
+
